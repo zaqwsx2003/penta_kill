30c890cff2de8543ec16152fafbecb71719778c1
littleduck <107936957+littleduck1219@users.noreply.github.com>
2024-05-16 17:31:06 +0900

    match page #5

--- app/(service)/(schedule)/_components/MatchCard.tsx
+++ app/(service)/(schedule)/_components/MatchCard.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import useKoreanDateFormat from "@/app/(service)/_lib/useDate";
-import React from "react";
+import React, { useState } from "react";
 import { Event } from "@/model/match";
 import TeamPanel from "./TeamPanel";
+import { ChevronDown } from "lucide-react";
 
 type MatchCardProps = {
     event: Event;
@@ -11,6 +12,18 @@ type MatchCardProps = {
 
 export default function MatchCard({ event }: MatchCardProps) {
     const KoreanDateFormat = (dates: string) => useKoreanDateFormat(dates);
+    const [isHovered, setIsHovered] = useState(false);
+    const [isChevronVisible, setIsChevronVisible] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+        setIsChevronVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+        setIsChevronVisible(false);
+    };
 
     return (
         <div className='flex justify-between flex-col gap-2' key={event.match.id}>
@@ -20,9 +33,21 @@ export default function MatchCard({ event }: MatchCardProps) {
                 {/* <span>{event.type}</span> */}
                 <span>{event.blockName}</span>
             </div>
-            <div className='flex'>
-                <TeamPanel event={event} position={0} />
-                <TeamPanel event={event} position={1} />
+            <div
+                className={`bg-gray-800 rounded-[25px] transition-all duration-300 ${
+                    isHovered ? "bg-gray-700" : ""
+                }`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>
+                <div className='flex'>
+                    <TeamPanel event={event} position={0} />
+                    <TeamPanel event={event} position={1} />
+                </div>
+                {isHovered && (
+                    <div className={`flex justify-center transition-all duration-300 z-50`}>
+                        <ChevronDown />
+                    </div>
+                )}
             </div>
         </div>
     );
--- app/(service)/(schedule)/_lib/useMatchPanelColor.tsx
+++ app/(service)/(schedule)/_lib/useMatchPanelColor.tsx
@@ -6,12 +6,10 @@ import { Result } from "@/model/match";
 export default function usePanelColor(teamResult: Result | null) {
     const panelColor = useMemo(() => {
         if (!teamResult || teamResult.outcome === null) {
-            return "bg-gray-800 opacity-1";
+            return "bg-gray-800 opacity-1 hover:bg-gray-500";
         }
 
-        return teamResult.outcome === "win"
-            ? "bg-green-500 hover:bg-green-400"
-            : "bg-gray-500 opacity-15";
+        return teamResult.outcome === "win" ? "bg-green-500" : "bg-gray-600 opacity-1";
     }, [teamResult]);
 
     return panelColor;
