b5e77bb2dccc13d3b4a0b4fd527c65bcda732796
solightnsalt <soldakm@gmail.com>
2024-06-26 02:02:03 +0900

    [Fix]update HTML attributes to Next.js conventions

--- app/(service)/(insert)/schedule/_components/MatchSchedule.tsx
+++ app/(service)/(insert)/schedule/_components/MatchSchedule.tsx
@@ -150,9 +150,9 @@ export default function MatchSchedule() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         className="lucide lucide-arrow-up"
                     >
                         <path d="m5 12 7-7 7 7" />
