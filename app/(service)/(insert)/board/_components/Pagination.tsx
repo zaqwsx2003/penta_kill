393947d1696979cb3f47d69e1ee149ea36d78b05
solightnsalt <soldakm@gmail.com>
2024-06-26 11:55:56 +0900

    [Fix]change the number of total pages

--- app/(service)/(insert)/board/_components/Pagination.tsx
+++ app/(service)/(insert)/board/_components/Pagination.tsx
@@ -1,28 +1,35 @@
 interface PaginationProps {
-	currentPage: number;
-	totalPages: number;
-	onPageChange: (page: number) => void;
+    currentPage: number;
+    totalPages: number;
+    onPageChange: (page: number) => void;
 }
 
-export default function Pagination({ currentPage, totalPages, onPageChange }: PaginationProps) {
-	const pageNumbers = [];
+export default function Pagination({
+    currentPage,
+    totalPages,
+    onPageChange,
+}: PaginationProps) {
+    const pageNumbers = [];
 
-	for (let i = 1; i <= totalPages; i++) {
-		pageNumbers.push(i);
-	}
+    for (let i = 1; i <= totalPages - 1; i++) {
+        pageNumbers.push(i);
+    }
 
-	return (
-		<div className="flex justify-center mt-4 text-xs">
-			{pageNumbers.map((number) => (
-				<button
-					key={number}
-					onClick={() => onPageChange(number)}
-					className={`px-4 py-2 mx-1 border rounded-full text-white ${
-						currentPage === number ? "bg-neutral-600" : " bg-transparent"
-					}`}>
-					{number}
-				</button>
-			))}
-		</div>
-	);
+    return (
+        <div className="mt-4 flex justify-center text-xs">
+            {pageNumbers.map((number) => (
+                <button
+                    key={number}
+                    onClick={() => onPageChange(number)}
+                    className={`mx-1 rounded-full border px-4 py-2 text-white ${
+                        currentPage === number
+                            ? "bg-neutral-600"
+                            : "bg-transparent"
+                    }`}
+                >
+                    {number}
+                </button>
+            ))}
+        </div>
+    );
 }
