7ffb6f5b2c6d378a16a4e2013ef0287e134ce4ab
littleduck <littleduck1219@gmail.com>
2024-07-02 19:18:27 +0900

    [Feat]emmiter on
    - sse emitter on

--- app/(service)/layout.tsx
+++ app/(service)/layout.tsx
@@ -6,7 +6,7 @@ export default async function Layout({ children }: LayoutProps) {
     return (
         <div className="flex min-h-[100vh] flex-col">
             <Header />
-            {/* <SSEProvider /> */}
+            <SSEProvider />
             <div className="">{children}</div>
         </div>
     );
